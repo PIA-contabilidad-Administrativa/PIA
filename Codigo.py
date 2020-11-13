56f356b90f48dc2e73ec32fb1157c1c7db84d96e
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-11-13 15:53:53 -0600

    cambio

--- Codigo.py
+++ Codigo.py
@@ -5,7 +5,12 @@ separador=("*"*30)
 menu=1
 listanombreproduc=[]
 listaunidadavender=[]
+listadelistas=[]
+listadelistas.append(listaunidadavender)
 contadorp=1
+contadoru=0
+primer="Primer Semestre"
+segundo="Segundo Semestre"
 while menu==1:
     print(separador+" Bienvenido al Programa "+separador)
     print("")
@@ -17,4 +22,14 @@ while menu==1:
         nombre=input(f"Dime el nombre del Producto{contadorp} :")
         listanombreproduc.append(nombre)
         contadorp=contadorp+1
+
+    for producto in listanombreproduc:
+        unidadavender=int(input(f"Dime las unidades a vender de el producto {listanombreproduc[contadoru]} del {primer}"))
+        preciodeventa=float(input(f"Dime el precio de Venta de el producto {listanombreproduc[contadoru]} del {primer}"))
+        contadoru=contadoru+1
+        unidadavender2=int(input(f"Dime las unidades a vender de el producto {listanombreproduc[contadoru]} del {segundo}"))
+        preciodeventa2=float(input(f"Dime el precio de Venta de el producto {listanombreproduc[contadoru]} del {segundo}"))
+        listaunidadavender.append(unidadavender,preciodeventa,unidadavender2,preciodeventa2)
+        
+
 
