b3c427779c14c559c87f4a7ed5a11b8401b7d9a5
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-11-13 15:19:50 -0600

    cambio

--- Codigo.py
+++ Codigo.py
@@ -4,8 +4,8 @@ import datetime
 separador=("*"*30)
 menu=1
 while menu==1:
-    print(separador+"Bienvenido al Programa"+separador)
+    print(separador+" Bienvenido al Programa "+separador)
     print("")
-    print("-"*15+"Este programa realiza el Presupuesto Maestro"+"-"*15)
+    print("-"*15+" Este programa realiza el Presupuesto Maestro "+"-"*15)
     print("")
     #Preguntar datos y agregar a variables
