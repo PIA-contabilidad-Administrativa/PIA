c0cefcdcc403ae4ca6abadef8703616977d524f1
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-11-13 15:14:17 -0600

    cambio

---
+++ Codigo.py
@@ -0,0 +1,9 @@
+import sys
+
+
+separador=("*"*30)
+menu=1
+while menu==1:
+    print(separador+"Bienvenido al Programa"+separador)
+    print("")
+    print("-"*15+"Este programa realiza el Presupuesto Maestro"+"-"*15)
