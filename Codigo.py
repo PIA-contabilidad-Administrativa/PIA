f14323ee108761752befb7eb21df93e5589cc3b6
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-11-13 15:34:27 -0600

    cambio

--- Codigo.py
+++ Codigo.py
@@ -3,9 +3,16 @@ import datetime
 
 separador=("*"*30)
 menu=1
+listanombreproduc=[]
+contadorp=1
 while menu==1:
     print(separador+" Bienvenido al Programa "+separador)
     print("")
     print("-"*15+" Este programa realiza el Presupuesto Maestro "+"-"*15)
     print("")
     #Preguntar datos y agregar a variables
+    productocon=int(input("Dime cuantos productos son :"))
+    for producto in productocon:
+        nombre=input(f"Dime el nombre del Producto{contadorp} :")
+        listanombreproduc.append(nombre)
+        contadorp=contadorp+1
