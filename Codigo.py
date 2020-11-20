6c62d42f2f3451796eb3acf01de6070fdc44c5ce
Luis Jair Alvarado Tobias <luisjair1394@gmail.com>
2020-11-19 19:12:27 -0600

    Tabla 7

--- Codigo.py
+++ Codigo.py
@@ -28,6 +28,10 @@ lista8_1=[]
 lista8_2=[]
 listareu=[]
 listanom8=[]
+lista_importeMOD_1=[]
+lista_total_horas_1=[]
+lista_importeMOD_2=[]
+lista_total_horas_2=[]
 contadorp=1
 contadoru=0
 contador3=0
@@ -53,6 +57,8 @@ contador22=1
 contador23=0
 contador24=0
 contador26=0
+contador27=0
+contador28=0
 contadorespecial=0
 primer="Primer Semestre"
 segundo="Segundo Semestre"
@@ -498,7 +504,69 @@ try:
         archivoA.close()
         
     
+        #Final Tabla 6
         
+        
+        #TABLA 7
+        
+        
+
+
+        for valor in (listanombreproduc):
+            
+            u_producir= int(input(f"Cuantas unidades se producieron primer semestre de {listanombreproduc[contador27]} : "))
+        
+            h_requeridas= float(input("Cuantas horas se ocuparon: "))
+    
+            cuota_hora= float(input("Cuota por hora: "))
+    
+            total_horas= (u_producir*h_requeridas)
+        
+            lista_total_horas_1.append(total_horas)
+    
+            importe_MOD= (total_horas*cuota_hora)
+    
+            lista_importeMOD_1.append(importe_MOD)
+    
+            T_horas_semestre_1= sum(lista_total_horas_1)
+    
+            Total_MOD1= sum(lista_importeMOD_1)
+    
+    
+            contador27= contador27 +1
+
+#print(T_horas_semestre_1)
+#print(Total_MOD1)
+
+        for valor in (listanombreproduc):
+            u_producir2= int(input(f"Cuantas unidades se producieron segundo semestre de {listanombreproduc[contador28]} : "))
+    
+            h_requeridas2= float(input("Cuantas horas se ocuparon: "))
+    
+            cuota_hora2= float(input("Cuota por hora: "))
+    
+            total_horas2= (u_producir2*h_requeridas2)
+        
+            lista_total_horas_2.append(total_horas2)
+    
+            importe_MOD2= (total_horas2*cuota_hora2)
+    
+            lista_importeMOD_2.append(importe_MOD2)
+    
+            T_horas_semestre_2= sum(lista_total_horas_2)
+    
+            Total_MOD2= sum(lista_importeMOD_2)
+    
+    
+            contador28= contador28 +1
+
+#print(T_horas_semestre_2)
+#print(Total_MOD2)
+
+        print(f"El total de horas requeridas  por semestre es de {T_horas_semestre_1 + T_horas_semestre_2}")
+
+        print(f"El total de M.O.D  por semestre es de {Total_MOD1 + Total_MOD2}")
+
 
 
 
