5286741d0383f2afa51abc85fd2af80bcfd9d854
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-11-20 21:57:24 -0600

    ultimo

--- Codigo.py
+++ Codigo.py
@@ -980,9 +980,12 @@ try:
         PTU=float(input("Dime el porcentaje de PTU (0.1) : "))
         archivoA.write("\n" )
         archivoA.write("VENTAS : "+str(VENTASTOTALES)+"\n" )
+
         archivoA.write("COSTO DE VENTAS : "+"AQUI VA LO DE LUIS"+"\n" )
         archivoA.write("UTILIDAD BRUTA : "+"VENTASTOTALES - COSTO DE VENTAS"+"\n" )
+
         archivoA.write("GASTO DE OPERACION : "+str(TOTALGIFF)+"\n" )
+
         archivoA.write("UTILIDAD DE OPERACION : "+"UTILIDAD BRUTA - GASTO DE OPERACION "+"\n" )
         archivoA.write("ISR : "+"UTILIDAD DE OPERACION  *  ISR "+"\n" )
         archivoA.write("PTU : "+"UTILIDAD DE OPERACION  *  PTU "+"\n" )
@@ -1052,6 +1055,139 @@ try:
         print("")
         print(separador)
         print("")
+
+
+
+        # BALANCE GENERAL 
+        print("")
+        print(separador)
+        archivoA=open("./respuestas.txt" , 'a')
+        archivoA.write("-"*15+"BALANCE GENERAL" +"-"*15 +"\n" )
+        print("-"*15+"BALANCE GENERAL" +"-"*15)
+        print("")
+        archivoA.write("\n")
+        archivoA.write("ACTIVO "+"\n")
+        archivoA.write("CIRCULANTE : "+"\n")
+        archivoA.write("\n")
+        archivoA.write("Efectivo : $ "+str(FEA)+"\n")
+        archivoA.write("Clientes : " +str(VENTASTOTALES)+"\n")
+
+        FYE=float(input("Dime los funcionarios y empleados : " ))
+        archivoA.write("Funcionarios y Empleados : " +str(FYE)+"\n")
+
+        DD=float(input("Dime los Deudores Diversos : "))
+        archivoA.write("Deudores Diversos : " +str(DD)+"\n")
+        archivoA.write("Inventario de Materiales : " +"IVENTARIO FINAL DE MATERIALES TABLA 11 LUIS "+"\n")
+        archivoA.write("Inventario de Producto termninado: " +"TABLA 11 LUIS "+"\n")
+
+
+        TAC=(FEA+VENTASTOTALES+FYE+DD)
+        #IVENTARIO DE MATERIALES + INVENTARIO DE PRODUCTO TERMINADO 
+        archivoA.write("TOTAL DE ACTIVO CIRCULANTE : " +str(TAC)+"\n")
+        archivoA.write("\n")
+        archivoA.write("NO CIRCULANTE : "+"\n")
+        archivoA.write("\n")
+
+        terreno=float(input("Dime el Terreno : "))
+        archivoA.write("Terreno : " +str(terreno)+"\n")
+
+        plantaye=float(input("Dime la Planta y Equipo : "))
+        PYE=(plantaye+CAFM)
+        archivoA.write("Planta y Equipo : " +str(PYE)+"\n")
+
+        depreciaciona=float(input("Dime la Depreciacion Acumulada : "))
+        DA=(depreciacion+depreciacion2-depreciaciona)
+
+        archivoA.write("Depreciacion Acumulada : " +str(DA)+"\n")
+
+        TANC=(terreno+PYE+DA)
+        archivoA.write("\n")
+        archivoA.write("TOTAL ACTIVO NO CIRCULANTE : " +str(TANC)+"\n")
+        archivoA.write("\n")
+        AT=(TAC+TANC)
+        archivoA.write("ACTIVO TOTAL : " +str(AT)+"\n")
+        archivoA.write("\n")
+
+        archivoA.write("PASIVO "+"\n")
+        archivoA.write("CORTO PLAZO : "+"\n")
+        archivoA.write("\n")
+        archivoA.write("Proveedores : " +str(saldo_proveedores_totales)+"\n")
+        documentospa=float(input("Dime los Documentos por Pagar : "))
+        archivoA.write("Documentos por Pagar : " +str(documentospa)+"\n")
+
+        archivoA.write("ISR por Pagar : " +"ISR LUIS "+"\n")
+        archivoA.write("PTU por Pagar : " +"PTU BALANCE GENERAL  LUIS "+"\n")
+
+        archivoA.write("\n")
+        TPCP=(saldo_proveedores_totales+documentospa)#ISR +PTU  luis
+        archivoA.write("TOTAL DE PASIVO CORTO PLAZO : "+str(TPCP)+"\n")
+        archivoA.write("\n")
+
+        archivoA.write("LARGO PLAZO : "+"\n")
+        archivoA.write("\n")
+
+        obligacionporpagar=float(input("Dime tus Obligaciones por pagar : "))
+        archivoA.write("Obligaciones por pagar : "+str(obligacionporpagar)+"\n")
+        archivoA.write("\n")
+
+        TPLP=(obligacionporpagar)
+
+        archivoA.write("TOTAL PASIVO LARGO PLAZO : "+str(obligacionporpagar)+"\n")
+        archivoA.write("\n")
+
+        PT=(TPCP+TPLP)
+        archivoA.write("PASIVO TOTAL : "+str(PT)+"\n")
+
+        archivoA.write("\n")
+
+        archivoA.write("CAPITAL CONTABLE : "+"\n")
+        archivoA.write("\n")
+
+        capaportado=float(input("Dime tu Capital Aportado : "))
+        archivoA.write("Capital Aportado : "+str(capaportado)+"\n")
+
+        capganado=float(input("Dime tu Capital Ganado : "))
+        archivoA.write("Capital Ganado : "+str(capganado)+"\n")
+
+
+        archivoA.write("Utlidad del Ejercicio : "+"utilidad neta"+"\n")#LUIS ESTADO DE RESULTADOS 
+
+
+        archivoA.write("\n")
+
+        TCC=(capaportado+capganado)# + UTILIDAD NETA 
+        archivoA.write("TOTAL DE CAPITAL CONTABLE  : "+"TCC"+"\n")
+        archivoA.write("\n")
+
+        SUMAPYC=(PT+TCC)
+        archivoA.write("SUMA DE PASIVO Y CAPITAL   : "+"SUMAPYC"+"\n")
+        archivoA.write("\n")
+        resultadofinal=(AT-SUMAPYC)
+        archivoA.write("\n")
+        archivoA.write("RESULTADO FINAL : "+str(resultadofinal)+"\n")
+
+        archivoA.write("///////////////////////////////////////////////////////////////" +"\n" )
+        archivoA.write("\n")
+        archivoA.close()
+        print("")
+        print(separador)
+        print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
